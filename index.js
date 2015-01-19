1e0c5c010f7a568d8d4849f1a67d724c50e78b52
Jean-Sébastien Franck <jsebfranck@gmail.com>
2015-01-19 16:12:22 +0100

    Express app with CSP config breaking Javascript on old Android browsers

---
+++ index.js
@@ -0,0 +1,23 @@
+var express = require('express'),
+  swig = require('swig')
+  helmetCsp = require('helmet-csp');
+
+var app = express();
+
+// Swig config
+app.engine('html', swig.renderFile);
+app.set('view engine', 'html');
+app.set('views', __dirname + '/views');
+app.set('view cache', false);
+
+// Helmet config
+app.use(helmetCsp({
+  scriptSrc: ['\'self\'']
+}));
+
+app.get('/', function (req, res) {
+  res.render('csp');
+});
+
+app.listen(3000);
+console.log('Application Started on http://localhost:3000/');
